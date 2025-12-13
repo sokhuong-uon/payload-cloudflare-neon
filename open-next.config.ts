1f3b920dd689508fee6812b3e95cf6ef9b605e8d
Sokhuong Uon <sokhuong.usk@gmail.com>
2025-12-13 17:18:59 +0700

    chore: cleanup opennext config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,3 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare'
-import type { OpenNextConfig } from '@opennextjs/cloudflare'
 
-const config = defineCloudflareConfig()
-
-export default {
-  ...config,
-  edgeExternals: [...(config.edgeExternals || []), 'jose'],
-} satisfies OpenNextConfig
+export default defineCloudflareConfig()
